347a941467767422fc52c9d59a5091cdacc16fed
Az400v <110109230+Az400v@users.noreply.github.com>
2022-08-14 22:20:50 +0530

    Update Program.cs

--- azArtifactConsume/Program.cs
+++ azArtifactConsume/Program.cs
@@ -4,8 +4,7 @@ namespace azArtifactConsume
 {
     class Program
     {
-      
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
