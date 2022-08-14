02e81c8e5b3cf3406e3ae7078cb2100efe2aebc0
Sonal <1000028538@hexaware.com>
2022-08-14 21:28:44 +0530

    Add project files.

---
+++ azArtifactConsume/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace azArtifactConsume
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
