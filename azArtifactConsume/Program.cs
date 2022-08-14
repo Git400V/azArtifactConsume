a10b89f94b4a1b3d72b8704cae664280af70c1d9
Sonal <1000028538@hexaware.com>
2022-08-14 21:31:57 +0530

    VS

--- azArtifactConsume/Program.cs
+++ azArtifactConsume/Program.cs
@@ -4,6 +4,7 @@ namespace azArtifactConsume
 {
     class Program
     {
+        //hi there from VS
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
