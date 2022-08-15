725fc7b71750d4755309cc0871131effc0386c23
Az400v <110109230+Az400v@users.noreply.github.com>
2022-08-15 09:07:51 +0530

    added comments sq2

--- azArtifactConsume/Program.cs
+++ azArtifactConsume/Program.cs
@@ -5,7 +5,7 @@ namespace azArtifactConsume
     class Program
     {
         
-
+        //sq merge
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
